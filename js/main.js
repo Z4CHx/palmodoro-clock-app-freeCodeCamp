edaee6fe92ca53b0185d19f29b368387dcb1d0f1
Zachary Durland <zacharydurland@gmail.com>
2017-04-21 15:47:09 +0900

    add new countdown logic
    - can add or subtract time dynamically

--- js/main.js
+++ js/main.js
@@ -4,27 +4,40 @@ var screen = $("div#timer-container #countdown");
 var startButton =$("div#countdown");
 var addButton =$("div#add-time");
 var subtractButton =$("div#subtract-time");
-var timeLeft = 5;
 var display = function(toDisplay){   
     screen.html(toDisplay);
 };
 
+var timeLeft = 5;
+var t;
+var countdown_is_on = 0;
 
+function countdown() {
+    display(timeLeft);
+    if (timeLeft !== 0) {
+        display(timeLeft);
+        console.log(timeLeft);
+        timeLeft--;
+        t = setTimeout(function(){ countdown() }, 1000);
+    } else {
+        stopCount();
+    }
+}
 
-var startTimer = function() {
-    setInterval(function(){
-        if (timeLeft === 0) {
-            clearInterval(startTimer);
-            
-        } else {       
-            timeLeft--;
-            display(timeLeft);
-        }
-    }, 1000);
-};
+function startCountdown() {
+    if (!countdown_is_on) {
+        countdown_is_on = 1;
+        countdown();
+    }
+}
 
-display(timeLeft);
-startButton.click(startTimer);
+function stopCount() {
+    clearTimeout(t);
+    countdown_is_on = 0;
+}
+
+display(timeLeft);    
+startButton.click(startCountdown);
 addButton.click(function(){
     timeLeft+=5;
     display(timeLeft);
