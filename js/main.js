d99c26a97cdc1814c4d3a3518c874c097bb77501
Zachary Durland <zacharydurland@gmail.com>
2017-04-23 13:25:55 +0900

    add background and complete sound effect
    - bug - changing to a preset time break countdown bar

--- js/main.js
+++ js/main.js
@@ -7,14 +7,20 @@ var subtractButton = $("div#subtract-time");
 var pauseButton =  $("div#pause");
 var restartButton =  $("div#reset");
 var presetButton = $(".timer-preset");
-var display = function(toDisplay){   
-    screen.html(toDisplay);
-};
-
-var timeLeft = 5;
+var timeLeft = 10;
 var t;
 var countdown_is_on = 0;
+var chosenTime = 10;
+var setChosenTime = function(time){
+    console.log("chosen time was " + time);
+    chosenTime = time;
+};
+var countdownBar = $("#countdown-bar");
 
+var finishedNotification = document.createElement('audio');
+finishedNotification.src = './media/timerdone.ogg';
+
+    
 function countdown() {
     display(timeLeft);
     if (timeLeft !== 0) {
@@ -24,6 +30,7 @@ function countdown() {
         t = setTimeout(function(){ countdown() }, 1000);
     } else {
         stopCount();
+        finishedNotification.play();
     }
 }
 
@@ -44,23 +51,30 @@ function startPreset() {
     display(timeLeft);
 }    
     
+var display = function(toDisplay){   
+    screen.html(toDisplay);
+    countdownBar.css('width', '' + timeLeft/chosenTime * 100 + '%');
+};
     
+display(timeLeft);
 
-display(timeLeft);    
 startButton.click(startCountdown);
 addButton.click(function(){
     timeLeft+=5;
+    setChosenTime(timeLeft);
     display(timeLeft);
 });
 subtractButton.click(function(){
     timeLeft-=5;
+    setChosenTime(timeLeft);
     display(timeLeft);
 });
 pauseButton.click(stopCount);
 restartButton.click(function(){
     timeLeft = 25;
+    setChosenTime(timeLeft);
     display(timeLeft);
 });
-presetButton.click(startPreset)
+presetButton.click(startPreset);
     
 });
