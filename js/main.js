6d0446073abfb73dc3bfe34721701e94ef9e7c1b
Zachary Durland <zacharydurland@gmail.com>
2017-04-23 19:57:52 +0900

    repair start button

--- js/main.js
+++ js/main.js
@@ -1,82 +1,82 @@
 $(document).ready(function(){
 
-var screen = $("div#timer-container #countdown");
-var startButton = $("div#countdown");
-var addButton = $("div#add-time");
-var subtractButton = $("div#subtract-time");
-var pauseButton =  $("div#pause");
-var restartButton =  $("div#reset");
-var presetButton = $(".timer-preset");
-var timeLeft = 10;
-var t;
-var countdown_is_on = 0;
-var chosenTime = 10;
-var setChosenTime = function(time){
-    console.log("chosen time was " + time);
-    chosenTime = time;
-};
-var countdownBar = $("#countdown-bar");
+    var screen = $("div#timer-container #countdown");
+    var startButton = $("div#countdown");
+    var addButton = $("div#add-time");
+    var subtractButton = $("div#subtract-time");
+    var pauseButton =  $("div#pause");
+    var restartButton =  $("div#reset");
+    var presetButton = $(".timer-preset");
+    var timeLeft = 10;
+    var t;
+    var countdown_is_on = 0;
+    var chosenTime = 10;
+    var setChosenTime = function(time){
+        console.log("chosen time was " + time);
+        chosenTime = time;
+    };
+    var countdownBar = $("#countdown-bar");
 
-var finishedNotification = document.createElement('audio');
-finishedNotification.src = './media/timerdone.ogg';
+    var finishedNotification = document.createElement('audio');
+    finishedNotification.src = './media/timerdone.ogg';
 
-    
-function countdown() {
-    display(timeLeft);
-    if (timeLeft !== 0) {
+
+    function countdown() {
         display(timeLeft);
-        console.log(timeLeft);
-        timeLeft--;
-        t = setTimeout(function(){ countdown() }, 1000);
-    } else {
-        stopCount();
-        finishedNotification.play();
+        if (timeLeft !== 0) {
+            display(timeLeft);
+            console.log(timeLeft);
+            timeLeft--;
+            t = setTimeout(function(){ countdown() }, 1000);
+        } else {
+            stopCount();
+            finishedNotification.play();
+        }
     }
-}
 
-function startCountdown() {
-    if (!countdown_is_on) {
-        countdown_is_on = 1;
-        countdown();
+    function startCountdown() {
+        if (!countdown_is_on) {
+            countdown_is_on = 1;
+            countdown();
+        }
     }
-}
 
-function stopCount() {
-    clearTimeout(t);
-    countdown_is_on = 0;
-}
-    
-function startPreset() {
-    timeLeft = parseInt(this.innerHTML);
-    setChosenTime(timeLeft);
-    display(timeLeft);
-}    
-    
-var display = function(toDisplay){   
-    screen.html(toDisplay);
-    console.log(timeLeft/chosenTime * 100);
-    countdownBar.css('width', '' + timeLeft/chosenTime * 100 + '%');
-};
-    
-display(timeLeft);
+    function stopCount() {
+        clearTimeout(t);
+        countdown_is_on = 0;
+    }
 
-startButton.click(startCountdown);
-addButton.click(function(){
-    timeLeft+=5;
-    setChosenTime(timeLeft);
+    function startPreset() {
+        timeLeft = parseInt(this.innerHTML);
+        setChosenTime(timeLeft);
+        display(timeLeft);
+    }    
+
+    var display = function(toDisplay){   
+        screen.html(toDisplay);
+        console.log(timeLeft/chosenTime * 100);
+        countdownBar.css('width', '' + timeLeft/chosenTime * 100 + '%');
+    };
+
     display(timeLeft);
-});
-subtractButton.click(function(){
-    timeLeft-=5;
-    setChosenTime(timeLeft);
-    display(timeLeft);
-});
-pauseButton.click(stopCount);
-restartButton.click(function(){
-    timeLeft = 10;
-    setChosenTime(timeLeft);
-    display(timeLeft);
-});
-presetButton.click(startPreset);
+
+    startButton.click(startCountdown);
+    addButton.click(function(){
+        timeLeft+=5;
+        setChosenTime(timeLeft);
+        display(timeLeft);
+    });
+    subtractButton.click(function(){
+        timeLeft-=5;
+        setChosenTime(timeLeft);
+        display(timeLeft);
+    });
+    pauseButton.click(stopCount);
+    restartButton.click(function(){
+        timeLeft = 10;
+        setChosenTime(timeLeft);
+        display(timeLeft);
+    });
+    presetButton.click(startPreset);
     
 });
